51dc7bf7d217d9ac6a32c3c5f5454aae4d15abf9
Jake Alagel <alagel.jacob@gmail.com>
2023-07-25 20:50:29 -0400

    initial functionality added, website and password fields with submit button

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [passwords, setPasswords] = useState({});
+  const [website, setWebsite] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleWebsiteChange = (event) => {
+    setWebsite(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setPasswords({ ...passwords, [website]: password });
+    setWebsite('');
+    setPassword('');
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Website:
+          <input type="text" value={website} onChange={handleWebsiteChange} />
+        </label>
+        <label>
+          Password:
+          <input
+            type="password"
+            value={password}
+            onChange={handlePasswordChange}
+          />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+      <ul>
+        {Object.entries(passwords).map(([website, password], index) => (
+          <li key={index}>
+            {website}: {password}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
